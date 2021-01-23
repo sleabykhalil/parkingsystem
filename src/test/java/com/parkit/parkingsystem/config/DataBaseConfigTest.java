b3f050ecdbd9546f7cd9619ccc2d7f42de65b1c5
sleabykhalil <sleabykhalil@users.noreply.github.com>
2021-01-23 23:07:41 +0100

    Mocking DataBaseConfig

--- src/test/java/com/parkit/parkingsystem/config/DataBaseConfigTest.java
+++ src/test/java/com/parkit/parkingsystem/config/DataBaseConfigTest.java
@@ -8,17 +8,23 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith({MockitoExtension.class, JMockitExtension.class})
 class DataBaseConfigTest {
 
     @Mock
     Connection connectionMock;
+
+    @Mock
+    PreparedStatement preparedStatementMock;
+
+    @Mock
+    ResultSet resultSetMock;
 
     DataBaseConfig dataBaseConfigUnderTest;
 
@@ -50,15 +56,36 @@ class DataBaseConfigTest {
     }
 
     @Test
-    void closeConnection_WhenConnectionNotNull_ShouldCloseConnection() {
+    void closeConnection_WhenConnectionNotNull_ShouldCloseConnection() throws SQLException, ClassNotFoundException {
+        //given create connection at setup
+
+        //when
+        Connection resultConnection;
+        resultConnection = dataBaseConfigUnderTest.getConnection();
+        dataBaseConfigUnderTest.closeConnection(resultConnection);
+
+        //then
+        verify(connectionMock, times(1)).close();
+        assertDoesNotThrow(() -> connectionMock.close());
 
     }
 
     @Test
-    void closePreparedStatement() {
+    void closePreparedStatement_WhenPreparedStatementNotNull_ShouldClosePreparedStatement() throws SQLException {
+        //given
+        //when
+        assertDoesNotThrow(()->dataBaseConfigUnderTest.closePreparedStatement(preparedStatementMock));
+        //then
+        verify(preparedStatementMock,times(1)).close();
     }
 
     @Test
-    void closeResultSet() {
+    void closeResultSet_WhenResultSetNotNull_ShouldCloseResultSet() throws SQLException {
+        //given
+        //when
+        assertDoesNotThrow(()->dataBaseConfigUnderTest.closeResultSet(resultSetMock));
+        //then
+        verify(resultSetMock,times(1)).close();
+
     }
 }
