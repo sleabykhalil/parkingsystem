7d3bee361495b44c48371a050e081583f3af5a58
sleabykhalil <sleabykhalil@users.noreply.github.com>
2021-01-23 23:14:50 +0100

    Correct Typo

--- src/main/java/com/parkit/parkingsystem/service/ParkingService.java
+++ src/main/java/com/parkit/parkingsystem/service/ParkingService.java
@@ -31,7 +31,7 @@ public class ParkingService {
         try {
             ParkingSpot parkingSpot = getNextParkingNumberIfAvailable();
             if (parkingSpot != null && parkingSpot.getId() > 0) {
-                String vehicleRegNumber = getVehichleRegNumber();
+                String vehicleRegNumber = getVehicleRegNumber();
                 // display Welcome message and 5% discount if the vehicle found in database
                 displayWelcomeMassageWithDiscount(vehicleRegNumber);
                 parkingSpot.setAvailable(false);
@@ -56,7 +56,7 @@ public class ParkingService {
         }
     }
 
-    private String getVehichleRegNumber() throws Exception {
+    private String getVehicleRegNumber() throws Exception {
         System.out.println("Please type the vehicle registration number and press enter key");
         return inputReaderUtil.readVehicleRegistrationNumber();
     }
@@ -101,7 +101,7 @@ public class ParkingService {
 
     public void processExitingVehicle() {
         try {
-            String vehicleRegNumber = getVehichleRegNumber();
+            String vehicleRegNumber = getVehicleRegNumber();
             Ticket ticket = ticketDAO.getTicket(vehicleRegNumber);
             Date outTime = new Date();
             ticket.setOutTime(outTime);
