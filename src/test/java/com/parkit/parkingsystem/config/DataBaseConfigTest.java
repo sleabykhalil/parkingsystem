06200cf9cd81ada2225b080de55630c484def4e2
sleabykhalil <khalilsleaby@hotmail.com>
2021-01-29 00:57:35 +0100

    Mocking static method with jmockito insted of using jmockit because ther is a conflict with jacoco

--- src/test/java/com/parkit/parkingsystem/config/DataBaseConfigTest.java
+++ src/test/java/com/parkit/parkingsystem/config/DataBaseConfigTest.java
@@ -1,20 +1,20 @@
 package com.parkit.parkingsystem.config;
 
-import mockit.MockUp;
-import mockit.integration.junit5.JMockitExtension;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.sql.*;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
-@ExtendWith({MockitoExtension.class, JMockitExtension.class})
+@ExtendWith(MockitoExtension.class)
 class DataBaseConfigTest {
 
     @Mock
@@ -26,66 +26,69 @@ class DataBaseConfigTest {
     @Mock
     ResultSet resultSetMock;
 
-    DataBaseConfig dataBaseConfigUnderTest;
-
-    @BeforeEach
-    void setUp() {
-        dataBaseConfigUnderTest = new DataBaseConfig();
-
-        // Mock static method DriverManager.getConnection
-        new mockit.MockUp<DriverManager>() {
-            @mockit.Mock
-            public Connection getConnection(String url, String user, String password) {
-                return connectionMock;
-            }
-        };
-    }
-
 
     @Test
     void getConnection_whenGetConnection_ConnectionReturn() throws SQLException, ClassNotFoundException {
         //given
 
-        //when
-        Connection resultConnection;
-        resultConnection = dataBaseConfigUnderTest.getConnection();
+        try (MockedStatic<DriverManager> mocked = Mockito.mockStatic(DriverManager.class)) {
+            DataBaseConfig dataBaseConfigUnderTest = new DataBaseConfig();
+            mocked.when(() -> DriverManager.getConnection("jdbc:mysql://localhost:3306/prod?serverTimezone=UTC",
+                    "root",
+                    "rootroot"))
+                    .thenReturn(connectionMock);
+            //when
+            Connection result = dataBaseConfigUnderTest.getConnection();
 
-        //then
-        assertNotNull(resultConnection);
-        assertEquals(connectionMock, resultConnection);
+            //then
+            mocked.verify(() -> DriverManager.getConnection("jdbc:mysql://localhost:3306/prod?serverTimezone=UTC",
+                    "root",
+                    "rootroot"));
+            assertThat(result).isEqualTo(connectionMock);
+        }
+
+
     }
 
     @Test
     void closeConnection_WhenConnectionNotNull_ShouldCloseConnection() throws SQLException, ClassNotFoundException {
-        //given create connection at setup
+        try (MockedStatic<DriverManager> mocked = Mockito.mockStatic(DriverManager.class)) {
+            DataBaseConfig dataBaseConfigUnderTest = new DataBaseConfig();
+            mocked.when(() -> DriverManager.getConnection("jdbc:mysql://localhost:3306/prod?serverTimezone=UTC",
+                    "root",
+                    "rootroot"))
+                    .thenReturn(connectionMock);
+            //when
+            Connection resultConnection = dataBaseConfigUnderTest.getConnection();
+            dataBaseConfigUnderTest.closeConnection(resultConnection);
 
-        //when
-        Connection resultConnection;
-        resultConnection = dataBaseConfigUnderTest.getConnection();
-        dataBaseConfigUnderTest.closeConnection(resultConnection);
-
-        //then
-        verify(connectionMock, times(1)).close();
-        assertDoesNotThrow(() -> connectionMock.close());
-
+            verify(connectionMock, times(1)).close();
+            assertDoesNotThrow(() -> connectionMock.close());
+        }
     }
 
     @Test
     void closePreparedStatement_WhenPreparedStatementNotNull_ShouldClosePreparedStatement() throws SQLException {
+
         //given
+        DataBaseConfig dataBaseConfigUnderTest = new DataBaseConfig();
+
         //when
-        assertDoesNotThrow(()->dataBaseConfigUnderTest.closePreparedStatement(preparedStatementMock));
+        assertDoesNotThrow(() -> dataBaseConfigUnderTest.closePreparedStatement(preparedStatementMock));
         //then
-        verify(preparedStatementMock,times(1)).close();
+        verify(preparedStatementMock, times(1)).close();
+
     }
 
     @Test
     void closeResultSet_WhenResultSetNotNull_ShouldCloseResultSet() throws SQLException {
         //given
+        DataBaseConfig dataBaseConfigUnderTest = new DataBaseConfig();
+
         //when
-        assertDoesNotThrow(()->dataBaseConfigUnderTest.closeResultSet(resultSetMock));
+        assertDoesNotThrow(() -> dataBaseConfigUnderTest.closeResultSet(resultSetMock));
         //then
-        verify(resultSetMock,times(1)).close();
+        verify(resultSetMock, times(1)).close();
 
     }
 }
